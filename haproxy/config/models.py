8d6a7a9d328b07475f7b33319b5101168d97b75e
Robert de Vries <r.devries@robbytu.net>
2016-11-05 18:10:29 +0100

    + Implemented simple configuration models

---
+++ haproxy/config/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Customer(models.Model):
+    name = models.CharField('Customer', max_length=100)
+    slug = models.CharField('Slug', max_length=32)
+
+
+class Site(models.Model):
+    MODES = (
+        ('tcp', 'TCP'),
+        ('http', 'HTTP')
+    )
+
+    customer = models.ForeignKey('Customer')
+    main_domain = models.CharField('Main domain', max_length=100)
+
+    mode = models.CharField('Mode', choices=MODES, max_length=4)
+
+    enable_http = models.BooleanField('Enable HTTP', default=True)
+
+    enable_https = models.BooleanField('Enable HTTPS', default=True)
+    force_https = models.BooleanField('Force HTTPS', default=True)
+    enable_sts = models.BooleanField('Enable STS', default=True)
+    enable_letsencrypt = models.BooleanField('Enable LetsEncrypt', default=True)
+
+    custom_configs = models.TextField('Custom backend config', null=True, blank=True)
+
+
+class Domain(models.Model):
+    site = models.ForeignKey('Site')
+    domain = models.CharField('Domain', max_length=100)
+
+
+class Server(models.Model):
+    site = models.ForeignKey('Site')
+    name = models.CharField('Server name', max_length=100, default='app')
+    address = models.CharField('Server address', max_length=100)
+    check = models.BooleanField('Check', default=True)
