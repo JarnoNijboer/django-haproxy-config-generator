1052d033d3ebf61784922113cfc9a9139c3d789f
Robert de Vries <r.devries@robbytu.net>
2016-11-05 19:37:13 +0100

    * Updated admin.py to reflect the model changes

--- haproxy/config/admin.py
+++ haproxy/config/admin.py
@@ -1,12 +1,24 @@
 from django.contrib import admin
-from models import Customer, Site
+from django.contrib.admin.options import InlineModelAdmin
+from models import Customer, Site, Domain
+
+
+class DomainListInline(InlineModelAdmin):
+    template = 'admin/InlineWithoutOriginal.html'
+    model = Domain
+    fields = ('domain', 'created', 'enabled',)
+    readonly_fields = ('created',)
+    can_delete = True
+    extra = 1
+    ordering = ('id',)
+    original = False
 
 
 class SiteAdmin(admin.ModelAdmin):
-    list_display = ('main_domain', 'customer', 'mode', 'enable_http', 'enable_https',)
+    list_display = ('__unicode__', 'customer', 'enabled', 'mode', 'enable_http', 'enable_https',)
     fieldsets = (
         (None, {
-            'fields': ('customer', 'mode', 'main_domain',)
+            'fields': ('mode', 'customer', 'enabled',)
         }),
         ('HTTP settings', {
             'fields': ('enable_http',),
@@ -14,7 +26,12 @@ class SiteAdmin(admin.ModelAdmin):
         ('HTTPS settings', {
             'fields': ('enable_https', 'force_https', 'enable_sts', 'enable_letsencrypt',),
         }),
+        ('Custom backend configuration', {
+            'classes': ('collapse',),
+            'fields': ('custom_configs',),
+        }),
     )
+    inlines = [DomainListInline]
 
 admin.site.register(Site, SiteAdmin)
 admin.site.register(Customer)
