00b2a332c63cd08bd79ada772bff707f143e4cac
Robert de Vries <r.devries@robbytu.net>
2016-11-05 18:19:18 +0100

    + Implemented basic ModelAdmin class for the Site model

--- haproxy/config/admin.py
+++ haproxy/config/admin.py
@@ -1,4 +1,20 @@
 from django.contrib import admin
-from models import Site
+from models import Customer, Site
 
-admin.site.register(Site)
+
+class SiteAdmin(admin.ModelAdmin):
+    list_display = ('main_domain', 'customer', 'mode', 'enable_http', 'enable_https',)
+    fieldsets = (
+        (None, {
+            'fields': ('customer', 'mode', 'main_domain',)
+        }),
+        ('HTTP settings', {
+            'fields': ('enable_http',),
+        }),
+        ('HTTPS settings', {
+            'fields': ('enable_https', 'force_https', 'enable_sts', 'enable_letsencrypt',),
+        }),
+    )
+
+admin.site.register(Site, SiteAdmin)
+admin.site.register(Customer)
